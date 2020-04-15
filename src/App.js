dde443f9647f1fe37c2478a56cbf16c9139801b4
Ngo Anh Canh <can@elcaNet.local>
2020-04-15 14:05:06 +0700

    master

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="footer">1653009 - Ngo Anh Canh</div>
+    </div>
+  );
+}
+
+export default App;
